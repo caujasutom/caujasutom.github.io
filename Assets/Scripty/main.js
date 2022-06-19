edc90c05580d82637212c16fc7ad301f2bba7d52
caujasutom <85121888+caujasutom@users.noreply.github.com>
2022-06-19 12:17:49 +0200

    Update main.js

--- Assets/Scripty/main.js
+++ Assets/Scripty/main.js
@@ -1,52 +1,25 @@
-function delay(time) {
-  return new Promise((resolve) => setTimeout(resolve, time))
+function delay(a) {
+  return new Promise(b => setTimeout(b, a))
 }
-
-var count = 0
-
+var count = 0;
 async function openNav() {
-  var b1 = document.querySelector(".hamburger-menu:hover .bar-top")
-  var b2 = document.querySelector(".hamburger-menu:hover .bar-middle")
-  var b3 = document.querySelector(".hamburger-menu:hover .bar-bottom")
-  var navbar = document.getElementsByClassName("navbar")
-  var a = document.querySelectorAll(".navbar a")
-  var ul = document.querySelector(".navbar ul")
-  var li = document.querySelectorAll(".navbar li")
-  var social = document.querySelector(".social")
-
-  if (count == 0) {
-    navbar[0].style.height = "100%"
-    b1.style.transform = "rotate(45deg)"
-    b2.style.transform = "translateX(1rem)"
-    b2.style.opacity = "0.0"
-    b3.style.transform = "rotate(-45deg)"
-
-    await delay(150)
-    ul.style.display = "flex"
-
-
-    for (var i = 0; i < li.length; i++) {
-      li[i].style.display = "flex"
-    }
-    for (var i = 0; i < a.length; i++) {
-      a[i].style.display = "flex"
-    }
-    count = 1
+  var e = document.querySelector(".hamburger-menu:hover .bar-top"),
+      b = document.querySelector(".hamburger-menu:hover .bar-middle"),
+      f = document.querySelector(".hamburger-menu:hover .bar-bottom"),
+      g = document.getElementsByClassName("navbar"),
+      c = document.querySelectorAll(".navbar a"),
+      h = document.querySelector(".navbar ul"),
+      d = document.querySelectorAll(".navbar li"),
+      i = document.querySelector(".social");
+  if (0 == count) {
+      g[0].style.height = "100%", e.style.transform = "rotate(45deg)", b.style.transform = "translateX(1rem)", b.style.opacity = "0.0", f.style.transform = "rotate(-45deg)", await delay(150), h.style.display = "flex";
+      for (var a = 0; a < d.length; a++) d[a].style.display = "flex";
+      for (var a = 0; a < c.length; a++) c[a].style.display = "flex";
+      count = 1
   } else {
-    navbar[0].style.height = "5em"
-    b1.style.transform = "rotate(0deg)"
-    b2.style.transform = "translateX(0rem)"
-    b2.style.opacity = "1.0"
-    b3.style.transform = "rotate(0deg)"
-
-    social.display = "none"
-    ul.style.display = "none"
-    for (var i = 0; i < li.length; i++) {
-      li[i].style.display = "none"
-    }
-    for (var i = 0; i < a.length; i++) {
-      a[i].style.display = "none"
-    }
-    count = 0
+      g[0].style.height = "5em", e.style.transform = "rotate(0deg)", b.style.transform = "translateX(0rem)", b.style.opacity = "1.0", f.style.transform = "rotate(0deg)", i.display = "none", h.style.display = "none";
+      for (var a = 0; a < d.length; a++) d[a].style.display = "none";
+      for (var a = 0; a < c.length; a++) c[a].style.display = "none";
+      count = 0
   }
 }
