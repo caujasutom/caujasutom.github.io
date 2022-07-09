b0a900337a984290c239a93df55ce8c7268594e1
caujasutom <85121888+caujasutom@users.noreply.github.com>
2022-07-09 18:07:31 +0200

    Add files via upload

--- Assets/Scripts/kocky.js
+++ Assets/Scripts/kocky.js
@@ -1,16 +1,23 @@
-var pc = document.getElementsByClassName('pribeh-container');
-for (var i = 0; i < vice.length; i++) {
-  pc[i].addEventListener("click", (e) => {
-    var pribeh = e.currentTarget.querySelector(".pribeh");
+if (window.innerWidth < 1100) {
+  var pc = document.getElementsByClassName("pribeh-container");
+  for (var i = 0; i < vice.length; i++) {
+    pc[i].addEventListener("click", (e) => {
+      var pribeh = e.currentTarget.querySelector(".pribeh");
+      var pribehz = e.currentTarget.querySelector(".vice #pribeh_of");
 
-    if (pribeh.classList.contains('zavrene')) {
-      pribeh.setAttribute("style", "display: block");
-      pribeh.classList.remove('zavrene');
-      pribeh.classList.add('opened');
-    } else {
-      pribeh.setAttribute("style", "display: none");
-      pribeh.classList.remove('opened');
-      pribeh.classList.add('zavrene');
-    }
-  });
+      if (pribeh.classList.contains("zavrene")) {
+        pribeh.setAttribute("style", "display: block");
+        pribeh.classList.remove("zavrene");
+        pribeh.classList.add("opened");
+        pribehz.innerHTML = "Skrýt příběh";
+      } else {
+        pribeh.setAttribute("style", "display: none");
+        pribeh.classList.remove("opened");
+        pribeh.classList.add("zavrene");
+        pribehz.innerHTML = "Zobrazit příběh";
+      }
+    });
+  }
+} else {
+  close();
 }
